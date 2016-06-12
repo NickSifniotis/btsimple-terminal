1d564f9e3a5b35fb07d8652b692a47a6456d76e8
nsifniotis <u5809912@anu.edu.au>
2016-06-12 13:02:38 +1000

    Menu and menu response code decompiled

--- DeviceListActivity.java
+++ DeviceListActivity.java
@@ -3,6 +3,9 @@ package au.net.nicksifniotis.btsimpleterminal;
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -58,4 +61,38 @@ public class DeviceListActivity extends Activity
             startActivityForResult(enableBtIntent, 1);
         }
     }
+
+    @Override
+    protected void onCreate (Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_device_list);
+
+        // the app rater function call is located here, but it won't be implemented because fuck that
+        // AppRater.app_launched (this);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu (Menu menu)
+    {
+        menu.add (0, 2, 2, "About");
+        return true;
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected (MenuItem item)
+    {
+        switch (item.getItemId())
+        {
+            case 2:
+                AboutDialog about = new AboutDialog(this);
+                about.setTitle ("About");
+                about.show();
+                break;
+        }
+        return true;
+    }
+
 }
